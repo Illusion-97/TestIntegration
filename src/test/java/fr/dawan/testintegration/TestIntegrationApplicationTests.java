a4f0ce410c6155cf1e632fb567aca22281aa0937
Yanis Adekalom <yanis971.60@gmail.com>
2023-06-27 09:17:17 +0200

    Initial commit

---
+++ src/test/java/fr/dawan/testintegration/TestIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.testintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
