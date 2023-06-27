c2bff8aa67b8d6d4c6b0e413bb1f011b6d84d757
Yanis Adekalom <yanis971.60@gmail.com>
2023-06-27 11:57:03 +0200

    Get Mapping

--- src/main/java/fr/dawan/testintegration/CalcController.java
+++ src/main/java/fr/dawan/testintegration/CalcController.java
@@ -1,9 +1,11 @@
 package fr.dawan.testintegration;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalcController {
+    @GetMapping
     public String index() {
         return "Welcome";
     }
